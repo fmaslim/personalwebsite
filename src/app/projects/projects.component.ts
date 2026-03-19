9108621a99f421f32f82923baa859fe153c11cc4
fmaslim <franky.maslim@yahoo.com>
2026-03-19 07:48:54 -0400

    Added interpolation, property binding, event binding click

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+    pageTitle = 'Projects';
+    pageSubTitle = 'A selection of work focused on systems, structure, and practical engineering';
+    featuredProject = 'Contoso Public Health IT';
 
+    bannerUrl = '/website-banner-2.png';
+    bannerAlt = 'Cloud computing and workspace banner';
+
+    showFeatured = true;
+
+    toggleFeatured() {
+        this.showFeatured = !this.showFeatured;
+    }
 }
